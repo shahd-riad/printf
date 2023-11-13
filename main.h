6fd936fb767ee700193970aa4486a5f45f50c3ed
shahd-riad <shahd.riad2344@gmail.com>
2023-11-13 08:18:45 -0800

    task0

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+int _printf(const char *format, ...);
+int _strlen(char *s);
+int _putchar(char c);
+
+#endif
---
+++ task0.c
@@ -0,0 +1,93 @@
+#include "main.h"
+
+/**
+ * _putchar - Writes a character to the standard output
+ * @c: The character to be written
+ *
+ * Return: On success, 1; otherwise, -1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _strlen - Calculates the length of a string
+ * @s: The string to calculate the length of
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s)
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+}
+
+/**
+ * _printf - Custom printf function
+ * @format: The format string
+ *
+ * Return: The number of characters printed (excluding null byte)
+ */
+int _printf(const char *format, ...)
+{
+	int len = 0;
+	char *str;
+	char ch;
+
+	va_list args;
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			_putchar(*format);
+			len++;
+			format++;
+		}
+		else
+		{
+			format++;
+			switch (*format)
+			{
+			case 'c':
+				ch = va_arg(args, int);
+				if ((ch >= 97 && ch <= 122) || (ch >= 65 && ch <= 90))
+					return (-1);
+				_putchar(ch);
+				len++;
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str == NULL)
+					write(1, "(null)", 6);
+				write(1, str, _strlen(str));
+				len += _strlen(str);
+				break;
+			case '%':
+				_putchar('%');
+				break;
+			case '\0':
+				return (-1);
+				break;
+			default:
+				_putchar('%');
+				_putchar(*format);
+				len += 2;
+				break;
+			}
+			format++;
+		}
+	}
+	va_end(args);
+	return (len);
+}
+
