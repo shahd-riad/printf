8606f39a2e67feb7112fac6a1f78cad001c4b4a5
shahd-riad <shahd.riad2344@gmail.com>
2023-11-13 10:32:16 -0800

    MOre

--- _printf.c
+++ _printf.c
@@ -9,10 +9,11 @@
 int _printf(const char *format, ...)
 {
     convert_match conversion[] = {
-        {"%s", printf_string}, {"%c", printf_char}};
+        {"%s", printf_string}, {"%c", printf_char},
+    };
 
     va_list args;
-    int i = 0, j, len = 0;
+    int i = 0, len = 0;
 
     va_start(args, format);
 
@@ -21,23 +22,34 @@ int _printf(const char *format, ...)
 
     while (format[i] != '\0')
     {
-        j = 1;
-        while (j <= 1)
+        if (format[i] == '%' && format[i + 1] != '\0')
         {
-            if (conversion[j].id[0] == format[i] && conversion[j].id[1] == format[1])
+            int j = 0;
+            while (j <= 1)
             {
-                len += conversion[j].f(args);
-                i += 2;
-                goto Here;
+                if (conversion[j].id[0] == format[i] && conversion[j].id[1] == format[i + 1])
+                {
+                    len += conversion[j].f(args);
+                    i += 2;
+                    break;
+                }
+                j++;
             }
-            j--;
         }
-        _putchar(*format);
-        len++;
-        i++;
+        else if (format[i] == '%' && format[i + 1] == '%')
+        {
+            _putchar('%');
+            len++;
+            i += 2;
+        }
+        else
+        {
+            _putchar(format[i]);
+            len++;
+            i++;
+        }
     }
 
-Here:
     va_end(args);
     return (len);
 }
