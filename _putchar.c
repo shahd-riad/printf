de3f21da90caac55b7003623c76f525d2ace6f02
Ahmed Abdelhafez <ahmedhafez.ta@gmail.com>
2023-11-14 13:36:38 -0800

    implement _puts function def inside _putchar source code

--- _putchar.c
+++ _putchar.c
@@ -11,3 +11,16 @@ int _putchar(char c)
 	return (write(1, &c, 1));
 }
 
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ * Return: number of chars written
+ */
+int _puts(char *str)
+{
+	register int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	return (i);
+}
